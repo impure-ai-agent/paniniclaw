57e0106e04111bce707e314bb969286dad8e2989
Andrew Zuo <andrew@andrewzuo.com>
2026-06-09 22:58:57 -0400

    mimo doesn't support tool calls apparently. Went back to qwen 3.5 flash as it's cheaper than deepseek

--- integrations/openrouter.go
+++ integrations/openrouter.go
@@ -19,7 +19,7 @@ type OpenRouter struct {
 func NewOpenRouter(apiKey string) *OpenRouter {
 	return &OpenRouter{
 		apiKey: apiKey,
-		model:  "xiaomi/mimo-v2.5",
+		model:  "qwen/qwen3.5-flash",
 	}
 }
 
